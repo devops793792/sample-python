2918829512e68b7a8c01f671efb561c73842fa6f
devops793792 <122431959+devops793792@users.noreply.github.com>
2023-08-30 22:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
